2d55de5c24c1e376841e23830fb0d10db9dbb41e
Welb César <cesarwelb@gmail.com>
2022-04-11 19:41:12 -0300

    build com correção para o github pages

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<ProfileInput />} />
-        <Route path="/users/:username" element={<UserDetails />} />
+        <Route path="/github_profiles/" element={<ProfileInput />} />
+        <Route
+          path="/github_profiles/users/:username"
+          element={<UserDetails />}
+        />
         <Route
           path="*"
           element={<Typography variant="h2">Página Não Encontrada</Typography>}
